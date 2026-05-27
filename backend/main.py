8afcc428fdf77698dbe71c2fe5aed53866974895
Chandan Kumar <chandan.amigo@gmail.com>
2026-05-27 07:22:02 +0530

    Added CORS support

--- backend/main.py
+++ backend/main.py
@@ -6,13 +6,15 @@ import models
 
 import reminder
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from schemas import JobCreate
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://job-tracker-iota-mauve.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
