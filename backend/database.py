cc17ac0bc8721469da81c3e847eb9394515ecc21
Chandan Kumar <chandan.amigo@gmail.com>
2026-05-27 07:58:37 +0530

    Fixed PostgreSQL connection

--- backend/database.py
+++ backend/database.py
@@ -8,7 +8,10 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 
 SessionLocal = sessionmaker(
