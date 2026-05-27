7fb480b0ed77ee9b7d1f7fbfec4c4e1139d0221b
Chandan Kumar <chandan.amigo@gmail.com>
2026-05-27 07:53:52 +0530

    Connected PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,21 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./jobs.db"
+import os
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 
 Base = declarative_base()
